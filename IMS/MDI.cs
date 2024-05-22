afe70f94ad4e229d164416487c033d019531172a
Saad Hussain <msh.2k21@gmail.com>
2024-05-22 21:41:19 +0500

    initial commit

---
+++ IMS/MDI.cs
@@ -0,0 +1,10 @@
+namespace IMS
+{
+    public partial class MDI : Form
+    {
+        public MDI()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IMS/Sample.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace IMS
+{
+    partial class Sample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.SteelBlue;
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Left;
+            panel1.ForeColor = Color.White;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 622);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(237, 237, 237);
+            panel2.Controls.Add(panel4);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Font = new Font("Microsoft Sans Serif", 8.25F);
+            panel2.Location = new Point(200, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(924, 622);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label1);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(200, 50);
+            panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label2);
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(924, 50);
+            panel4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Right;
+            label1.Font = new Font("Malgun Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(100, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.Dock = DockStyle.Left;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 50);
+            label2.TabIndex = 1;
+            label2.Text = "USER";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // Sample
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1124, 622);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Sample";
+            Text = "Sample";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
+        private Panel panel4;
+        private Label label1;
+        private Label label2;
+    }
+}
